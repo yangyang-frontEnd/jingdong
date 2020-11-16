0321753dab216c41fc89c732b8c5c1c66895beff
yangyang <1434753182@qq.com>
2020-11-16 14:37:53 +0800

    首页-布局01

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import RouterComponent from "./router"
+import RouterComponent from "./router";
 
-import "./assets/css/common/public.css"
+import "./assets/css/common/public.css";
 // import "./assets/js/libs/zepto"
 
+import "swiper/css/swiper.min.css";
+import "swiper/js/swiper.min.js";
+
 ReactDOM.render(
-    <React.StrictMode>
-        <RouterComponent/>
-    </React.StrictMode>,
-    document.getElementById("root")
+  <React.StrictMode>
+    <RouterComponent />
+  </React.StrictMode>,
+  document.getElementById("root")
 );
--- src/pages/home/index/index.js
+++ src/pages/home/index/index.js
@@ -1,13 +1,37 @@
-import React, {useEffect} from 'react'
+import React, { useEffect } from "react";
+import Css from "../../../assets/css/home/index/index.module.css";
+import Swiper from "swiper";
 
 function IndexComponent() {
-    useEffect(() => {
-    }, [])
-    return (
-        <div>
-            我是首页
+  useEffect(() => {
+    new Swiper(`.${Css["swiper-wrap"]}`, {
+      pagination: {
+        el: ".swiper-pagination",
+      },
+    });
+  }, []);
+  return (
+    <div>
+      <div className={Css["search-header"]}>
+        <div className={Css["classify-icon"]}></div>
+        <div className={Css["search-wrap"]}>
+          <div className={Css["search-icon"]}></div>
+          <div className={Css["search-text"]}>请输入宝贝名称</div>
         </div>
-    )
+        <div className={Css["login-wrap"]}>
+          <div className={Css["login-text"]}>登录</div>
+        </div>
+      </div>
+      <div className={Css["swiper-wrap"]}>
+        <div className="swiper-wrapper">
+          <div className="swiper-slide">slider1</div>
+          <div className="swiper-slide">slider2</div>
+          <div className="swiper-slide">slider3</div>
+        </div>
+        <div className="swiper-pagination"></div>
+      </div>
+    </div>
+  );
 }
 
-export default IndexComponent
+export default IndexComponent;
